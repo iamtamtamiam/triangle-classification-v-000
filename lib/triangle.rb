d899da1d97a70fcdbd1f24e3bc3ec9fd19bf9c15
Tam Vu <vutam_a@hotmail.com>
2019-07-06 00:26:24 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,7 +3,7 @@ class Triangle
   attr_accessor :a, :b, :c 
   
   def initialize(a, b, c)
-    if a <= 0 || b <= 0 || c <= 0 || a + b < c || a + c < b || b + c < a
+    if a <= 0 || b <= 0 || c <= 0 || a + b <= c || a + c <= b || b + c <= a
       raise TriangleError
     else 
       @a = a 
